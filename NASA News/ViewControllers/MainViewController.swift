2d8e3a7fb89d75a42ea24c70b0883d8c28d2bc82
Alex Umarov <umarov.iosdev@gmail.com>
2020-08-15 00:04:51 +0300

    MainViewController

--- NASA News/ViewControllers/MainViewController.swift
+++ NASA News/ViewControllers/MainViewController.swift
@@ -7,14 +7,55 @@
 //
 
 import UIKit
-
-class MainViewController: UIViewController {
-
+final class MainViewController: UITableViewController {
+    //MARK: - Properties
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    var news = [Model]()
+    var networkManager = NetworkManager()
+    
+    //Mark: - Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
     }
-
-
+    
+   private func setupView() {
+        networkManager.fetchData()
+        activityIndicator.startAnimating()
+        delay(1) {
+            self.news = self.networkManager.newsList
+            if self.news.isEmpty {
+                self.networkAlert()
+            } else {
+                self.loadData()
+            }
+        }
+        navigationController?.navigationBar.barStyle = .black
+    }
+    //MARK: - TableView
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return news.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Appearance.StringValues.cellName) as! NewsDataCell
+        cell.titleLabel?.text = news[indexPath.row].title
+        cell.date_createdLabel?.text = news[indexPath.row].date_created
+        cell.imageCell?.sd_setImage(with: URL(string: news[indexPath.row].href!),
+                                    placeholderImage: UIImage(named: news[indexPath.row].href!)
+        )
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        if let indexPath = self.tableView.indexPathForSelectedRow{
+            let selectedRow = indexPath.row
+            let dvc = segue.destination as! DetailsViewController
+            dvc.titleText = self.news[selectedRow].title
+            dvc.imageURL = self.news[indexPath.row].href
+            dvc.newsText = self.news[indexPath.row].description
+        }
+    }
 }
 
