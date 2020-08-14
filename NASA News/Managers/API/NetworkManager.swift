b1aae1dd0308943b0d1f672a8b75804dc5cef6e3
Alex Umarov <umarov.iosdev@gmail.com>
2020-08-14 23:36:49 +0300

    Network manager and Model

--- NASA News/Managers/API/NetworkManager.swift
+++ NASA News/Managers/API/NetworkManager.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2020 Александр Умаров. All rights reserved.
 //
 
-import Foundation
+import SwiftyJSON
+class NetworkManager {
+    var newsList = [Model]()
+    
+    func fetchData() {
+        let url = URL(string: Appearance.StringValues.baseUrl + "\(getDate())")
+        guard let downloadURL = url else {return}
+        let session = URLSession.shared
+        
+        session.dataTask(with: downloadURL) { data, response, error in
+            guard let data = data else {return}
+            
+            do {
+                let myJSON = try JSON(data: data)
+                let items = myJSON[Appearance.JSONames.collection][Appearance.JSONames.items]
+                
+                for item in items.arrayValue {
+                    let title = item[Appearance.JSONames.data][0][Appearance.JSONames.title].stringValue
+                    let nasa_id = item[Appearance.JSONames.data][0][Appearance.JSONames.nasa_id].stringValue
+                    let description = item[Appearance.JSONames.data][0][Appearance.JSONames.description].stringValue
+                    let href = item[Appearance.JSONames.links][0][Appearance.JSONames.href].stringValue
+                    let date_created = item[Appearance.JSONames.data][0][Appearance.JSONames.dateCreated].stringValue
+                    
+                    self.newsList.append(Model(nasa_id: nasa_id,
+                                               title: title,
+                                               date_created: self.getStringFormattedDate(dateCreated: date_created),
+                                               media_type: "",
+                                               href: href,
+                                               description: description,
+                                               date_created_sort: self.getFormattedDate(dateCreated: date_created)))
+                }
+                self.newsList.sort(by: { $0.date_created_sort! > $1.date_created_sort! } )
+            } catch { print(error) }
+        }.resume()
+    }
+}
--- NASA News/Models/Model.swift
+++ NASA News/Models/Model.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+struct Model {
+    var nasa_id: String?
+    var title: String
+    var date_created: String?
+    var media_type: String?
+    var href: String?
+    var description: String?
+    var date_created_sort: Date?
+    
+    init(nasa_id: String, title: String, date_created: String, media_type: String, href: String, description: String, date_created_sort: Date) {
+        self.href = href
+        self.nasa_id = nasa_id
+        self.title = title
+        self.date_created = date_created
+        self.media_type = media_type
+        self.description = description
+        self.date_created_sort = date_created_sort
+    }
+    
+}
