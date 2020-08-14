4e8e458eb5e1116272004adb7833d72fd8554ed2
Alex Umarov <umarov.iosdev@gmail.com>
2020-08-14 23:34:07 +0300

    base structure commit

---
+++ NASA News/Managers/API/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  NASA News
+//
+//  Created by Александр Умаров on 14.08.2020.
+//  Copyright © 2020 Александр Умаров. All rights reserved.
+//
+
+import Foundation
---
+++ NASA News/ViewControllers/DetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailsViewController.swift
+//  NASA News
+//
+//  Created by Александр Умаров on 14.08.2020.
+//  Copyright © 2020 Александр Умаров. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
